b690577640ef2e95f36ecce925ecc6e4b729f8ee
Raul Cisneros <avfreq@gmail.com>
2021-11-14 21:20:41 -0600

    added daily, weekly, monthly, data to all sections

---
+++ js/data.js
@@ -0,0 +1,60 @@
+const daily = function() {
+    document.querySelector(".data-hrs-work").innerHTML = "5hrs";
+    document.querySelector(".data-previous-work").innerHTML = "This week - 7hrs";
+
+    document.querySelector(".data-hrs-play").innerHTML = "1hrs";
+    document.querySelector(".data-previous-play").innerHTML = "This week - 2hrs";
+
+    document.querySelector(".data-hrs-study").innerHTML = "0hrs";
+    document.querySelector(".data-previous-study").innerHTML = "This week - 1hrs";
+
+    document.querySelector(".data-hrs-exercise").innerHTML = "1hrs";
+    document.querySelector(".data-previous-exercise").innerHTML = "This week - 1hrs";
+
+    document.querySelector(".data-hrs-social").innerHTML = "1hrs";
+    document.querySelector(".data-previous-social").innerHTML = "This week - 3hrs";
+
+    document.querySelector(".data-hrs-self-care").innerHTML = "0hrs";
+    document.querySelector(".data-previous-self-care").innerHTML = "This week - 1hrs";
+}
+
+const weekly = function() {
+    document.querySelector(".data-hrs-work").innerHTML = "32hrs";
+    document.querySelector(".data-previous-work").innerHTML = "Last week - 36hrs";
+
+    document.querySelector(".data-hrs-play").innerHTML = "10hrs";
+    document.querySelector(".data-previous-play").innerHTML = "Last week - 8hrs";
+
+    document.querySelector(".data-hrs-study").innerHTML = "4hrs";
+    document.querySelector(".data-previous-study").innerHTML = "Last week - 7hrs";
+
+    document.querySelector(".data-hrs-exercise").innerHTML = "4hrs";
+    document.querySelector(".data-previous-exercise").innerHTML = "Last week - 5hrs";
+
+    document.querySelector(".data-hrs-social").innerHTML = "5hrs";
+    document.querySelector(".data-previous-social").innerHTML = "Last week - 10hrs";
+
+    document.querySelector(".data-hrs-self-care").innerHTML = "2hrs";
+    document.querySelector(".data-previous-self-care").innerHTML = "Last week - 2hrs";
+}
+
+const monthly = function() {
+    document.querySelector(".data-hrs-work").innerHTML = "103hrs";
+    document.querySelector(".data-previous-work").innerHTML = "Last month - 128hrs";
+
+    document.querySelector(".data-hrs-play").innerHTML = "23hrs";
+    document.querySelector(".data-previous-play").innerHTML = "Last month - 29hrs";
+
+    document.querySelector(".data-hrs-study").innerHTML = "13hrs";
+    document.querySelector(".data-previous-study").innerHTML = "Last month - 19hrs";
+
+    document.querySelector(".data-hrs-exercise").innerHTML = "11hrs";
+    document.querySelector(".data-previous-exercise").innerHTML = "Last month - 18hrs";
+
+    document.querySelector(".data-hrs-social").innerHTML = "21hrs";
+    document.querySelector(".data-previous-social").innerHTML = "Last month - 23hrs";
+
+    document.querySelector(".data-hrs-self-care").innerHTML = "7hrs";
+    document.querySelector(".data-previous-self-care").innerHTML = "Last month - 11hrs";
+}
+
